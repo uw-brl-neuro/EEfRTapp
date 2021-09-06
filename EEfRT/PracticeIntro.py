44b935afb50f224e8fea3c60c8596567f908fbc8
Davin05 <86816313+Davin05@users.noreply.github.com>
2021-09-05 18:37:28 -0700

    Updates on PracticeIntro.py
    - Add comments
    - Change button name

--- EEfRT/PracticeIntro.py
+++ EEfRT/PracticeIntro.py
@@ -4,8 +4,10 @@ from tkinter import font as tkFont
 import StartEndPage
 import Trial
 
+# keep track of how many practice trials have passed
 global number_of_practice
 
+# Create the introductory page to the practice trial session
 class PracticeIntro(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -13,8 +15,14 @@ class PracticeIntro(tk.Frame):
         subFrame = tk.Frame(master=self)
         subFrame.pack()
 
+        # Do not collect data from the trial at this phae
         Trial.start_collect = False
+        # Exclude the number of practice trials from the count of total trials
         Trial.trial_number = 0 - master.get_maximum_practice()
+
+        # Reset the number of practice that has given to the participant to 0
+        global number_of_practice
+        number_of_practice = 0
 
         lbl = tk.Label(subFrame,
                        text = f"Before we officially begin, you would play {master.get_maximum_practice()} practice trials",
@@ -25,20 +33,24 @@ class PracticeIntro(tk.Frame):
 
         lbl2.grid(row = 1, column = 1)
 
+        # Button to Start Page
         btn_to_startpage = ttk.Button(subFrame,
                                       text = "Previous Page",
                                       command = lambda : master.switch_frame(StartEndPage.StartPage))
         btn_to_startpage.grid(row = 2, column = 0)
 
+        # Button to the practice trial page
         btn_to_trial = ttk.Button(subFrame,
                                   text="Next Page",
                                   command=lambda: master.switch_frame(PracticeTrial))
         btn_to_trial.grid(row= 2, column=2)
 
+# Create an intro page for each practice trial and show the number of current practice trial
 class PracticeTrial(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
+        # Increase the number of practice trials given to the participant by 1
         global number_of_practice
         number_of_practice += 1
 
@@ -50,14 +62,17 @@ class PracticeTrial(tk.Frame):
                        font= tkFont.Font(size=25))
         lbl.grid(row = 0, column = 1)
 
+        # Button to the introductory page of the practice trial session
         btn_to_PracticeIntro = ttk.Button(subFrame,
-                                          text="Previous Page",
+                                          text="Restart Practice",
                                           command=lambda: master.switch_frame(PracticeIntro))
         btn_to_PracticeIntro.grid(row=2, column=0)
 
+        # Button to begin this practice trial
         btn_to_trial = ttk.Button(subFrame,
                                   text="Confirm",
                                   command=lambda: master.switch_frame(Trial.TrialCue))
         btn_to_trial.grid(row=2, column=2)
+
 
 
