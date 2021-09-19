3bdda5e0e6d58275e00f9b7066e3e4d1a6d3adbf
Davin05 <86816313+Davin05@users.noreply.github.com>
2021-09-18 19:41:03 -0700

    add autotest

---
+++ EEfRT/TestAutotest.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font as tkFont
+import TestMenu
+import time
+import datetime
+import random
+import StartEndPage
+
+
+class Autotest(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+
+        subFrame = tk.Frame(master=self)
+        subFrame.pack()
+
+        # Generate a reward based on the range given by the configuration
+        def reward_generator(master):
+            global current_reward
+            current_reward = random.uniform(master.get_reward_lowerbound(), master.get_reward_upperbound()).__round__(2)
+            return current_reward
+
+        # Generate the probability of winning the current trial based on the configuration
+        def probability_generator(master):
+            global probability_to_win
+            probability_to_win = random.choice(master.get_probability())
+            return probability_to_win
+
+        lbl = tk.Label(subFrame,
+                       text="TEST MODE", font=tkFont.Font(size=(master.get_font_size() - 5)),
+                       fg="red")
+        lbl.grid(row=0, column=0)
+
+        progress = ttk.Progressbar(subFrame,
+                                   orient=tk.HORIZONTAL, length=500, maximum = TestMenu.number_of_entry + 1,
+                                   mode='determinate')
+        progress.grid(row=1, column=0)
+
+        StartEndPage.participant_name = "T-Admin"
+
+        for i in range(TestMenu.number_of_entry):
+            master.new_data()
+            master.record_data(i + 1)
+            ts = time.time()
+            master.record_data(datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+            master.record_data(probability_generator(master))
+            task_level = random.choice([0, 1])
+            master.record_data(task_level)
+            if task_level == 0:
+                master.record_data(1)
+            else:
+                master.record_data(reward_generator(master))
+            master.record_data(random.choice([0, 1]))
+            master.record_data(random.choice([0, 1]))
+            master.data_merge()
+            progress.step(1)
+
+
+        self.after(0, lambda: master.switch_frame(StartEndPage.EndPage))
+
--- EEfRT/TestMenu.py
+++ EEfRT/TestMenu.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import font as tkFont
-import StartEndPage
 import Trial
+import TestAutotest
 
 # keep track of how many practice trials have passed
-global number_of_practice
+global number_of_entry
 
 # Create the introductory page to the practice trial session
 class TestMenu(tk.Frame):
@@ -24,22 +24,30 @@ class TestMenu(tk.Frame):
         # Collect data from the trial at this phase
         Trial.start_collect = True
 
+        global number_of_entry
         number_entry = tk.Entry(subFrame)
-        number_entry.grid(row=2, column=0)
+        number_entry.grid(row=3, column=0)
 
         lbl2 = tk.Label(subFrame,
                        text=f"Test Menu",
                        font=tkFont.Font(size=master.get_font_size()))
         lbl2.grid(row=1, column=0)
 
+        lbl3 = tk.Label(subFrame,
+                        text=f"How many auto test do you want to run?",
+                        font=tkFont.Font(size=master.get_font_size() - 5))
+        lbl3.grid(row=2, column=0)
+
         btn_to_page1 = ttk.Button(subFrame,
                                   text="Confirm",
                                   command=lambda: self.autotest(master, number_entry))
-        btn_to_page1.grid(row=3, column=0)
+        btn_to_page1.grid(row=4, column=0)
 
 
-    def autotest(self, master, number_entry):
-        master.destroy()
+    def autotest(self, master, entry):
+        global number_of_entry
+        number_of_entry = int(entry.get())
+        self.after(0, lambda: master.switch_frame(TestAutotest.Autotest))
 
 
 
